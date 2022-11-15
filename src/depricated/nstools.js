079d3d8ccc9d8d3f8d36da85c01343ee81bdf559
TheKarmicChaos <73192767+TheKarmicChaos@users.noreply.github.com>
2022-11-15 07:58:21 -0500

    Move PeekPort & UpdPort to depricated files

--- src/depricated/nstools.js
+++ src/depricated/nstools.js
@@ -1,3 +1,67 @@
 import * as tb from "/lib/toolbox";
 import Queue from "/lib/classes";
 import { getNsDataThroughFile, runCommand, checkNsInstance } from "/lib/helpers";
+
+/** Returns all data in a port without removing it.
+ * @param {import("../..").NS} ns
+ * @param {number} port - Number of the port you want to peek.
+ * - 1: Bitnode & Run Info
+ * - 2: Income ($/sec)
+ * - 3: Hashes
+ * - 6: Run Progress
+ * - 7: Gang
+ * - 8: Corporation
+ * - 9: BladeBurner
+ * @param {string} extraFunc - Exta function to parse the data further before returning.
+ * - "none": Default value, does nothing.
+ * - "sumdict": Use on a dict port to return the sum of all values in the dict.
+ * */
+export function PeekPort(ns, port, extraFunc = "none") {
+	checkNsInstance(ns, 'PeekPort');
+	// get total income
+	let portData = ns.readPort(port);
+	ns.getPortHandle(port).write(portData);
+	switch (extraFunc) {
+		case "none":
+			return portData;
+		case "sumdict":
+			let total = 0;
+			for (let entry in portData) { total += portData[entry]; };
+			return total;
+	}
+}
+
+/** Updates specified data in a port.
+ * @param {import("../..").NS} ns
+ * @param {number} port - Number of the port you want to peek.
+ * - 1: Bitnode & Run Info
+ * - 2: Income ($/sec)
+ * - 3: Hashes
+ * - 6: Run Progress
+ * - 7: Gang
+ * - 8: Corporation
+ * - 9: BladeBurner
+ * @param {string} dataStruct - The type of data structure expected to be in this port.
+ * - Default value: "array"
+ * - Possible values: "dict"
+ * @param {any[]} args - An array containing the update data.
+ * - "dict": Array contains a list of strings for each key to update in the dict,
+ * with each key immediately followed by the value for that key.
+ * - Example: ["key1", 10, "key2", true, "key3", "redpill"]
+ * */
+export function UpdPort(ns, port, dataStruct = "array", args = []) {
+	checkNsInstance(ns, 'UpdPort');
+	let portData = ns.readPort(port);
+	switch (dataStruct) {
+		case "arr" || "array":
+			try { portData = portData.concat(args); }
+			catch (err) { ns.toast("ERROR: UpdPort was passed invalid args array for the specified dataStruct.") };
+			break;
+		case "dict" || "dictionary":
+			try { while (args.length > 0) { portData[args.shift()] = args.shift(); }; }
+			catch (err) { ns.toast("ERROR: UpdPort was passed invalid args array for the specified dataStruct.") };
+			break;
+	}
+	ns.getPortHandle(port).write(portData);
+	return portData;
+}
--- src/lib/nstools.js
+++ src/lib/nstools.js
@@ -80,70 +80,6 @@ export async function RunCom(ns, command, argList, shouldreturn = true) {
 	//await runCommand(ns, String(realcommand), String(filePath), argList);
 }
 
-/** Returns all data in a port without removing it.
- * @param {import("../..").NS} ns
- * @param {number} port - Number of the port you want to peek.
- * - 1: Bitnode & Run Info
- * - 2: Income ($/sec)
- * - 3: Hashes
- * - 6: Run Progress
- * - 7: Gang
- * - 8: Corporation
- * - 9: BladeBurner
- * @param {string} extraFunc - Exta function to parse the data further before returning.
- * - "none": Default value, does nothing.
- * - "sumdict": Use on a dict port to return the sum of all values in the dict.
- * */
-export function PeekPort(ns, port, extraFunc = "none") {
-	checkNsInstance(ns, 'PeekPort');
-	// get total income
-	let portData = ns.readPort(port);
-	ns.getPortHandle(port).write(portData);
-	switch (extraFunc) {
-		case "none":
-			return portData;
-		case "sumdict":
-			let total = 0;
-			for (let entry in portData) { total += portData[entry]; };
-			return total;
-	}
-}
-
-/** Updates specified data in a port.
- * @param {import("../..").NS} ns
- * @param {number} port - Number of the port you want to peek.
- * - 1: Bitnode & Run Info
- * - 2: Income ($/sec)
- * - 3: Hashes
- * - 6: Run Progress
- * - 7: Gang
- * - 8: Corporation
- * - 9: BladeBurner
- * @param {string} dataStruct - The type of data structure expected to be in this port.
- * - Default value: "array"
- * - Possible values: "dict"
- * @param {any[]} args - An array containing the update data.
- * - "dict": Array contains a list of strings for each key to update in the dict,
- * with each key immediately followed by the value for that key.
- * - Example: ["key1", 10, "key2", true, "key3", "redpill"]
- * */
-export function UpdPort(ns, port, dataStruct = "array", args = []) {
-	checkNsInstance(ns, 'UpdPort');
-	let portData = ns.readPort(port);
-	switch (dataStruct) {
-		case "arr" || "array":
-			try { portData = portData.concat(args); }
-			catch (err) { ns.toast("ERROR: UpdPort was passed invalid args array for the specified dataStruct.") };
-			break;
-		case "dict" || "dictionary":
-			try { while (args.length > 0) { portData[args.shift()] = args.shift(); }; }
-			catch (err) { ns.toast("ERROR: UpdPort was passed invalid args array for the specified dataStruct.") };
-			break;
-	}
-	ns.getPortHandle(port).write(portData);
-	return portData;
-}
-
 /** Returns the average gains per second for a specified stat from a specified crime.
  * @param {import("../..").NS} ns
  * @param {string} crime - Name of crime.
@@ -289,7 +225,6 @@ export async function GetJobGains(ns, company, stat, sleeveid = -1) {
 	}
 	return gainsPerSec;
 };
-
 
 /** Retrieves the global variable dictionary from global-vars.txt
  * @param {import("../..").NS} ns
