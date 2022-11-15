5ef0d66a11721f5fb6d753a13d7f2322078e5b89
TheKarmicChaos <73192767+TheKarmicChaos@users.noreply.github.com>
2022-11-15 11:41:05 -0500

    Convert nstools from .js to .tsx

--- src/lib/nstools.tsx
+++ src/lib/nstools.tsx
@@ -1,5 +1,6 @@
 import * as tb from "/lib/toolbox";
 import Queue from "/lib/classes";
+import { NS } from "@ns";
 import { getNsDataThroughFile, runCommand, checkNsInstance } from "/lib/helpers";
 
 // -------------------------------------------------------------------------------------
@@ -7,19 +8,18 @@ import { getNsDataThroughFile, runCommand, checkNsInstance } from "/lib/helpers"
 // -------------------------------------------------------------------------------------
 
 /** Runs a command remotely, so it doesn't eat up RAM.
- * @param {import("../..").NS} ns
- * @param {string} command - String of an actual function, with no args.
+ * @param command - String of an actual function, with no args.
  * - Example: "ns.getServerMaxRam()"
- * @param {any[]} argList - Array of args to pass into the function.
- * @param {boolean} shouldreturn - Default value: true
+ * @param argList - Array of args to pass into the function.
+ * @param shouldreturn - Default value: true
  * - Bool for whether we can expect a return value from running the command.
  * - ALWAYS set to false if running a command that has no return value.
  * */
-export async function RunCom(ns, command, argList, shouldreturn = true) {
+export async function RunCom(ns: NS, command: string, argList?: any[], shouldreturn: boolean = true): Promise<any> {
 	checkNsInstance(ns, 'RunCom');
 	let basecommand = command.split("(")[0]
 
-	let filePath = null //'/MiscTemp/' + basecommand.slice(3) + ".txt"
+	let filePath = undefined //'/MiscTemp/' + basecommand.slice(3) + ".txt"
 
 	var realcommand = basecommand + "("
 	if (argList != null) {
@@ -46,19 +46,18 @@ export async function RunCom(ns, command, argList, shouldreturn = true) {
 /** Returns an array with the following 2 elements:
  * - 0: An array of every server in the game.
  * - 1: A dictionary with the key/values as serv/parentserv for all servs.
- * @param {import("../..").NS} NS
- * @param {boolean} includePservs - Default value: false
+ * @param includePservs - Default value: false
  * - Determines whether the result should include servers purchased by the player.
- * @param {boolean} includeHnodes - Default value: false
+ * @param includeHnodes - Default value: false
  * - Determines whether the result should include hacknet node servers.
  * */
-export function ScanAll(ns, includePservs = false, includeHnodes = false) {
+export function ScanAll(ns: NS, includePservs: boolean = false, includeHnodes: boolean = false): (string[] | Record<string, undefined | string>)[] {
 	checkNsInstance(ns, 'ScanAll');
-	const pservs = [];
-	const hservs = [];
-	var servQueue = new Queue();
-	let seenServs = ['home'];
-	let servDict = { 'home': null };
+	const pservs: string[] = new Array();
+	const hservs: string[] = new Array();
+	let seenServs: string[] = ['home'];
+	var servQueue: Queue = new Queue();
+	let servDict: Record<string, undefined | string> = { 'home': undefined };
 
 	ScanServ('home'); //ns.print(servQueue)
 	while (servQueue.isEmpty() == false) { ScanServ(servQueue.dequeue()) } // while the queue is not empty, scan the next serv in queue
@@ -68,7 +67,7 @@ export function ScanAll(ns, includePservs = false, includeHnodes = false) {
 	return [ seenServs, servDict ];
 
 	// This subfunction scans the server 'node' for any new neighbors to add to the queue updating servQueue and seenServs as needed
-	function ScanServ(node) {
+	function ScanServ(node: string) {
 		var neighbors = ns.scan(node); // scans the current node to get a list of neighbors
 		if (!(neighbors[0] == null)) { // if list of neighbors is not empty
 			for (let servername of neighbors) { // for each neighboring server
@@ -89,15 +88,14 @@ export function ScanAll(ns, includePservs = false, includeHnodes = false) {
 // -------------------------------------------------------------------------------------
 
 /** Returns the average gains per second for a specified stat from a specified crime.
- * @param {import("../..").NS} ns
- * @param {string} crime - Name of crime.
- * @param {string} stat - The name of the stat of which to calculate the avg gains per second.
+ * @param crime - Name of crime.
+ * @param stat - The name of the stat of which to calculate the avg gains per second.
  * - Possible values: "karma", "money", "kills", "hacking_exp", "strength_exp", "defense_exp", "dexterity_exp", "agility_exp",
  * "charisma_exp", "intelligence_exp"
- * @param {number} crimeChance - The chance of the given crime succeeding. Leave as default value to let the function calculate this for you.
+ * @param crimeChance - The chance of the given crime succeeding. Leave as default value to let the function calculate this for you.
  * - If using this function for sleeves, you must calculate this chance yourself and pass it into the function.
  * */
-export async function GetCrimeGains(ns, crime, stat, crimeChance = -1) {
+export async function GetCrimeGains(ns: NS, crime: string, stat: string, crimeChance: number = -1): Promise<number> {
 	checkNsInstance(ns, 'GetCrimeGains');
 	if (crimeChance == -1) crimeChance = await RunCom(ns, 'ns.singularity.getCrimeChance()', [crime]);
 	const crimeStats = await RunCom(ns, 'ns.singularity.getCrimeStats()', [crime])
@@ -113,12 +111,11 @@ export async function GetCrimeGains(ns, crime, stat, crimeChance = -1) {
 }
 
 /** Returns the chance of a given sleeve succeeding at committing a given crime.
- * @param {import("../..").NS} ns
- * @param {number} id - ID number of the sleeve committing the crime.
- * @param {string} crime - The name of the crime the sleeve would be committing.
+ * @param id - ID number of the sleeve committing the crime.
+ * @param crime - The name of the crime the sleeve would be committing.
  * - Possible values: "MUG", "HOMICIDE"
  * */
-export async function GetSleeveCrimeChance(ns, id, crime) {
+export async function GetSleeveCrimeChance(ns: NS, id: number, crime: string): Promise<number> {
 	checkNsInstance(ns, 'GetSleeveCrimeChance');
 	const slstats = await RunCom(ns, 'ns.sleeve.getSleeveStats()', [id])
 	let slchance = 0;
@@ -145,16 +142,15 @@ export async function GetSleeveCrimeChance(ns, id, crime) {
 }
 
 /** Returns the average gains per second for a specified stat for your job at a specified company.
- * @param {import("../..").NS} ns
- * @param {string} company - Name of company (must have a job here).
- * @param {string} stat - The name of the stat of which to calculate the avg gains per second.
+ * @param company - Name of company (must have a job here).
+ * @param stat - The name of the stat of which to calculate the avg gains per second.
  * - Possible values: "money", "hacking_exp", "strength_exp", "defense_exp", "dexterity_exp", "agility_exp",
  * "charisma_exp", "intelligence_exp"
  * - "rep" not yet supported.
- * @param {number} sleeveid - The numerical ID of the sleeve working this job.
+ * @param sleeveid - The numerical ID of the sleeve working this job.
  * - Leave as default value if the player is the one working.
  * */
-export async function GetJobGains(ns, company, stat, sleeveid = -1) {
+export async function GetJobGains(ns: NS, company: string, stat: string, sleeveid: number = -1): Promise<number> {
 	checkNsInstance(ns, 'GetJobGains');
 	const player = await RunCom(ns, "ns.getPlayer()");
 	let skills; let mults;
@@ -168,19 +164,17 @@ export async function GetJobGains(ns, company, stat, sleeveid = -1) {
 	const companyPositionName = player.jobs[company];
 	const companyPositionData = tb.GetBaseJobData(companyPositionName);
 	const salaryMult = 1; const expMult = 1;
+	const sourceFiles: (Record<string, number>)[] = await RunCom(ns, 'ns.singularity.getOwnedSourceFiles()')
+	const bnMults: Record<string, number> = await RunCom(ns, 'ns.getBitNodeMultipliers()')
 
 	// If player has SF-11, calculate salary multiplier from favor
 	let favor = await RunCom(ns, "ns.singularity.getCompanyFavor()", [company])
 	let favorMult = 1 + favor / 100;
-	if (isNaN(favorMult)) {
-	  favorMult = 1;
-	}
-	let hasSF11 = false
-	for (let sf in PeekPort(ns, 1)["sourceFiles"]) { if (sf["n"] == 11 && sf["lvl"] > 0) hasSF11 = true; }
+	if (isNaN(favorMult)) { favorMult = 1; }
+	let hasSF11 = false;
 	let bn11Mult = 1;
-	if (hasSF11) {
-	  bn11Mult = favorMult;
-	}
+	for (let sf of sourceFiles) { if (sf["n"] == 11 && sf["lvl"] > 0) hasSF11 = true; }
+	if (hasSF11) { bn11Mult = favorMult; }
 
 	/*
 	let jobPerformance = companyPosition.calculateJobPerformance(
@@ -198,34 +192,34 @@ export async function GetJobGains(ns, company, stat, sleeveid = -1) {
 	let gainsPerSec = 0;
 	switch (stat) {
 		case "money":
-			gainsPerSec = companyPositionData.baseSalary * salaryMult * mults.work_money * PeekPort(ns, 1)["mults"].CompanyWorkMoney * bn11Mult;
+			gainsPerSec = companyPositionData.baseSalary * salaryMult * bnMults.work_money * mults.CompanyWorkMoney * bn11Mult;
 			break;
 		case "rep":
 			gainsPerSec = jobPerformance * mults.company_rep * favorMult;
 			break;
 		case "hacking_exp":
 			let hackExpGain = companyPositionData.hackingExpGain || 0
-			gainsPerSec = hackExpGain * expMult * mults.hacking_exp * PeekPort(ns, 1)["mults"].CompanyWorkExpGain;
+			gainsPerSec = hackExpGain * expMult * mults.hacking_exp * bnMults.CompanyWorkExpGain;
 			break;
 		case "strength_exp":
 			let strExpGain = companyPositionData.strengthExpGain || 0
-			gainsPerSec = strExpGain * expMult * mults.strength_exp * PeekPort(ns, 1)["mults"].CompanyWorkExpGain;
+			gainsPerSec = strExpGain * expMult * mults.strength_exp * bnMults.CompanyWorkExpGain;
 			break;
 		case "defense_exp":
 			let defExpGain = companyPositionData.defenseExpGain || 0
-			gainsPerSec = defExpGain * expMult * mults.defense_exp * PeekPort(ns, 1)["mults"].CompanyWorkExpGain;
+			gainsPerSec = defExpGain * expMult * mults.defense_exp * bnMults.CompanyWorkExpGain;
 			break;
 		case "dexterity_exp":
 			let dexExpGain = companyPositionData.dexterityExpGain || 0
-			gainsPerSec = dexExpGain * expMult * mults.dexterity_exp * PeekPort(ns, 1)["mults"].CompanyWorkExpGain;
+			gainsPerSec = dexExpGain * expMult * mults.dexterity_exp * bnMults.CompanyWorkExpGain;
 			break;
 		case "agility_exp":
 			let agiExpGain = companyPositionData.agilityExpGain || 0
-			gainsPerSec = agiExpGain * expMult * mults.agility_exp * PeekPort(ns, 1)["mults"].CompanyWorkExpGain;
+			gainsPerSec = agiExpGain * expMult * mults.agility_exp * bnMults.CompanyWorkExpGain;
 			break;
 		case "charisma_exp":
 			let chaExpGain = companyPositionData.charismaExpGain || 0
-			gainsPerSec = chaExpGain * expMult * mults.charisma_exp * PeekPort(ns, 1)["mults"].CompanyWorkExpGain;
+			gainsPerSec = chaExpGain * expMult * mults.charisma_exp * bnMults.CompanyWorkExpGain;
 			break;
 		case "intelligence_exp":
 			gainsPerSec = 0;
@@ -238,23 +232,20 @@ export async function GetJobGains(ns, company, stat, sleeveid = -1) {
 // Global Variable Functions
 // -------------------------------------------------------------------------------------
 
-/** Retrieves the global variable dictionary from global-vars.txt
- * @param {import("../..").NS} ns
- * */
-export function getGlobals(ns) {
+/** Retrieves the global variable dictionary from global-vars.txt */
+export function getGlobals(ns: NS): Record<string, any> {
 	checkNsInstance(ns, 'getGlobals');
 	const fileData = ns.read('global-vars.txt');
     return JSON.parse(fileData); // Deserialize it back into an object/array and return
 };
 
 /** Updates global variables and writes them to global-vars.txt
- * @param {import("../..").NS} ns
- * @param {any[]} args - An array containing the update data.
+ * @param args - An array containing the update data.
  * - The array contains a list of strings for each key to update in the dict,
  * with each key immediately followed by the value for that key.
  * - Example: ["key1", 10, "category1.key2", true, "key17", "redpill"]
  * */
-export function updGlobals(ns, args) {
+export function updGlobals(ns: NS, args: any[]): void {
 	checkNsInstance(ns, 'updGlobals');
 	let curDict = getGlobals(ns);
 	try {
