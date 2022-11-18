86981f58ff767455ec711422a0810bb4e5711fa8
TheKarmicChaos <73192767+TheKarmicChaos@users.noreply.github.com>
2022-11-18 17:00:36 -0500

    Lower wait timer at start of new run.

--- src/initRun.js
+++ src/initRun.js
@@ -1,7 +1,7 @@
 /** @param {import("../").NS} ns */
 export async function main(ns) {
 	ns.killall("home");
-	await ns.sleep(2900);
+	await ns.sleep(900);
 	ns.run('hud.js', 1, "clear");
 	await ns.sleep(100);
 	ns.run('main.js');
