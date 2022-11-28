66498b0b9c3780a1035a2c2cf694b22bd2e05dea
TheKarmicChaos <73192767+TheKarmicChaos@users.noreply.github.com>
2022-11-27 21:28:04 -0500

    Support for challenge runs & missing SFs

--- src/main.js
+++ src/main.js
@@ -2,65 +2,58 @@ import * as nstb from "./lib/nstools";
 import * as tb from "./lib/toolbox";
 
 /** This main script determines what type of run this is, what strategies are valid in this BN, etc. and runs their related automation files accordingly.
+ * - NOTE: main.js, and by extention my entire script setup, cannot run without SF 4x3 (for singularity functions) and SF 5x1 (for getBitNodeMultipliers).
  * @param {import("../").NS} ns */
 export async function main(ns) {
 	ns.closeTail(); await ns.sleep(1); ns.tail('main.js'); await ns.sleep(1); ns.resizeTail(340, 600); await ns.sleep(1); ns.moveTail(1560, 120);
 	ns.disableLog("ALL"); ns.clearLog();
 
+	// Kill all core scripts & liquidate stocks
 	ns.killall('home');
 	try {
 		ns.killall('hacknet-node-0');
 		ns.exec('lp_stockmaster.js', 'hacknet-node-0', 1, '-l');
  	} catch (err) { }
 
+	// Settings - Customize what can be used this run (For challenges or un-owned SFs)
+	// ============================================================
+	const allowBHome = true;	// Can automate buying RAM/core upgrades for home computer
+	const allowB4s = true;		// Can automate buying 4s Data API
+
+	const allowGang = true;		// Can automate gangs (requires SF 2x1)
+	const allowCorp = true;		// Can automate corps (requires SF 3x3)
+	const allowBB = true;		// Can automate bladeburner (requires SF 7x1)
+	const allowHacknet = true;	// Can automate hacknet (requires SF 9x1)
+	const allowSleeves = true;	// Can automate sleeves (requires SF 10x1)
+	const allowGraft = true;	// Can automate grafting (requires SF 10x1)
+	const allowStanek = false;	// Can automate stanek (requires SF 13x1)
+
+
+	// Initialize Variables
+	// ============================================================
 	let player = await nstb.RunCom(ns, 'ns.getPlayer()');
-	var bitNode = player["bitNodeN"]; let bName;
-	let sourceFiles = await nstb.RunCom(ns, 'ns.singularity.getOwnedSourceFiles()')
-	let bndata = await nstb.RunCom(ns, 'ns.getBitNodeMultipliers()')
+	var bitNode = player["bitNodeN"];
+	let sourceFiles = await nstb.RunCom(ns, 'ns.singularity.getOwnedSourceFiles()');
+	let bndata = await nstb.RunCom(ns, 'ns.getBitNodeMultipliers()');
 	let runType = "redpill";
 	let PREV_VARS = nstb.getGlobals(ns);
 	let GLOBAL_VARS;
 
-	let numnodes = await nstb.RunCom(ns, 'ns.hacknet.numNodes()')
-	let inGang = await nstb.RunCom(ns, "ns.gang.inGang()")
+	let numnodes;
+	if (allowHacknet) numnodes = await nstb.RunCom(ns, 'ns.hacknet.numNodes()');
+	let inGang;
+	if (allowGang) inGang = await nstb.RunCom(ns, "ns.gang.inGang()")
 
-	let scriptsToRun = []; let scriptsRunning = [];
-	let hnscriptsToRun = []; let hnscriptsRunning = [];
-	let maxRamForScripts; let coreScriptsSize;
+	let scriptsToRun = [];
+	let scriptsRunning = [];
+	let hnscriptsToRun = [];
+	let hnscriptsRunning = [];
+	let maxRamForScripts;
+	let coreScriptsSize;
 	let hnscriptsSize = 0;
 
-
-	// Initialize all global vars
-	// ============================================================
-
-	let globalDict = {
-		bnMults: {},
-		hackMult: 0,
-		sourceFiles: [],
-		bitNode: 0,
-		loop: 0,
-		runType: "",
-		want4s: true,
-		income: { base: 0, hacknet: 0, gang: 0, corp: 0, playerCrime: 0, sleeveCrime: 0, playerWork: 0, sleeveWork: 0, hacking: 0, bladeburner: 0 },
-		hash: { count: 0, income: 0, max: 1 },
-		sleeve: { shock: 100 },
-		gang: { want: false, has: false, territory: 0, respect: 0},
-		corp: { want: false, has: false, hasProd: false, hasLab: false, hasTAII: false, research: 0, funds: 0, profit: 0, products: [] },
-		bb: { want: false, has: false, hasSimu: false, city: "Sector-12", doneOps: ["failsafe"], isComplete: false },
-		backdoors: ["home"]
-	}
-	globalDict.bnMults = bndata;
-	globalDict.sourceFiles = sourceFiles;
-	globalDict.bitNode = bitNode;
-
-	// Preserve loop counter if we didn't just switch bitnodes.
-	if (PREV_VARS && (PREV_VARS["bitNode"] === undefined || PREV_VARS["bitNode"] == bitNode)) globalDict.loop = PREV_VARS["loop"];
-
-
-	// SETTINGS & SETUP
-	// ============================================================
-
 	// Determine the name of the bitnode
+	let bName;
 	switch (bitNode) {
 		case 1:
 			bName = "Source Genesis"; break;
@@ -90,7 +83,34 @@ export async function main(ns) {
 			bName = "They're lunatics"; break;
 	}
 
-	// Update some global vars
+	// Initialize Global Vars
+	// ============================================================
+
+	let globalDict = {
+		bnMults: {},
+		hackMult: 0,
+		sourceFiles: [],
+		bitNode: 0,
+		loop: 0,
+		runType: "",
+		want4s: true,
+		income: { base: 0, hacknet: 0, gang: 0, corp: 0, playerCrime: 0, sleeveCrime: 0, playerWork: 0, sleeveWork: 0, hacking: 0, bladeburner: 0 },
+		hash: { count: 0, income: 0, max: 1 },
+		sleeve: { shock: 100 },
+		gang: { want: false, has: false, territory: 0, respect: 0},
+		corp: { want: false, has: false, hasProd: false, hasLab: false, hasTAII: false, research: 0, funds: 0, profit: 0, products: [] },
+		bb: { want: false, has: false, hasSimu: false, city: "Sector-12", doneOps: ["failsafe"], isComplete: false },
+		backdoors: ["home"]
+	}
+	
+	globalDict.bnMults = bndata;
+	globalDict.sourceFiles = sourceFiles;
+	globalDict.bitNode = bitNode;
+
+	// Preserve loop counter if we didn't just switch bitnodes.
+	if (PREV_VARS && (PREV_VARS["bitNode"] === undefined || PREV_VARS["bitNode"] == bitNode)) globalDict.loop = PREV_VARS["loop"];
+
+	// Set hackMult
 	if (bndata.HackingLevelMultiplier > 0
 		&& bndata.HackExpGain > 0
 		&& bndata.ServerGrowthRate > 0
@@ -101,33 +121,37 @@ export async function main(ns) {
 		&& bndata.ScriptHackMoneyGain > 0) {
 		globalDict.hackMult = bndata.ServerGrowthRate * bndata.ServerMaxMoney * bndata.ServerWeakenRate * bndata.ScriptHackMoneyGain;
 	}
-	if (bndata.CorporationValuation > 0
-		&& bndata.CorporationSoftcap > 0) {
+
+	// Set Corp "want" & "has"
+	if (allowCorp && bndata.CorporationValuation * bndata.CorporationSoftcap > 0) {
 		if (player.hasCorporation) globalDict.corp.has = true;
 		else if (bndata.CorporationValuation >= 0.35) globalDict.corp.want = true;
 	}
-	if (bndata.GangSoftcap > 0
-		&& bndata.GangUniqueAugs > 0) {
+
+	// Set Gang "want" & "has"
+	if (allowGang && bndata.GangSoftcap * bndata.GangUniqueAugs > 0) {
 		if (inGang) globalDict.gang.has = true;
 		else globalDict.gang.want = true;
 	}
-	if (bndata.FourSigmaMarketDataApiCost > 4
+
+	// Set "want4s"
+	if (!allowB4s
+		|| bndata.FourSigmaMarketDataApiCost > 4
 		|| bndata.FourSigmaMarketDataCost > 5
 		|| ns.stock.has4SDataTIXAPI()) {
 		globalDict.want4s = false;
 	}
 
 	// Figure out which runType works best for this BN
-	if (bndata.DaedalusAugsRequirement <= 60 && bitNode != 6 && bitNode != 7) {
-		runType = "redpill"
-	} else if (bndata.BladeburnerRank > 0) {
-		runType = "bladeburner"
+	if (allowBB && (bitNode == 6 || bitNode == 7 || bndata.DaedalusAugsRequirement > 60)) {
+		runType = "bladeburner";
 		globalDict.bb.want = true;
-	}
+	} else { runType = "redpill"; }
+	globalDict.runType = runType;
 
-	// Update the last few initial global vars then write to file.
-	globalDict.runType = runType
+	// Write global vars to file.
 	ns.write("global-vars.txt", JSON.stringify(globalDict), "w")
+
 
 	// ===========================================================================================================
 	// MAIN WHILE LOOP
@@ -136,7 +160,7 @@ export async function main(ns) {
 	while (true) {
 		// update player
 		player = await nstb.RunCom(ns, 'ns.getPlayer()')
-		numnodes = await nstb.RunCom(ns, 'ns.hacknet.numNodes()')
+		if (allowHacknet) numnodes = await nstb.RunCom(ns, 'ns.hacknet.numNodes()')
 		GLOBAL_VARS = nstb.getGlobals(ns);
 		// clear any leftover banked strats
 		scriptsToRun = [];
@@ -158,70 +182,64 @@ export async function main(ns) {
 		// Strat Banking
 		// ========================================================================================================================
 		// ========================================================================================================================
+
+		if (bndata.CrimeMoney > 0 || (allowGang && bndata.GangSoftcap * bndata.GangUniqueAugs > 0)) 
+			BankStrat('op_task-manager.js');
+		if (allowBHome)
+			BankStrat('op_bupgr.js');
+		if (allowHacknet && bndata.HacknetNodeMoney > 0)
+			BankStrat('op_bhnodes.js');
+		
 		switch (runType) {
-			default:
-				if (bndata.CrimeMoney > 0 || bndata.GangSoftcap * bndata.GangUniqueAugs > 0) 
-					BankStrat('op_task-manager.js');
-				BankStrat('op_bupgr.js');
-				if (bndata.HacknetNodeMoney > 0) 
-					BankStrat('op_bhnodes.js');
-				if (player.money >= 2e6)
-					BankStrat('lp_stockmaster.js');
-				if (bndata.PurchasedServerLimit > 0)
-					BankStrat('op_bservs.js');
-				BankStrat('lp_svhack.js');
-				BankStrat('mp_nuke-backdoor.js');
-				BankStrat('op_sleeve.js');
-				if (bndata.CodingContractMoney > 0)
-					BankStrat('cct_solve.js');
-				BankStrat('op_joinfacs.js');
-				BankStrat('op_graft.js');
-				if (bndata.GangSoftcap * bndata.GangUniqueAugs > 0)
-					BankStrat('lp_gang.js');
-				if (bndata.CorporationValuation * bndata.CorporationSoftcap > 0)
-					BankStrat('mp_corp.js');
-				if (GLOBAL_VARS["hackMult"] > 0)
-					BankStrat('lp_loopmaster.js');
-
-				if (player.skills.hacking >= 3000 * bndata.WorldDaemonDifficulty || GLOBAL_VARS["bb"]["isComplete"]) {
-					let myAugs = await nstb.RunCom(ns, 'ns.singularity.getOwnedAugmentations()');
-					if (myAugs.includes("The Red Pill") || GLOBAL_VARS["bb"]["isComplete"]) { BankStrat('endBitNode.js') }
-				};
-				break;
-
 			case "bladeburner":
-				if (bndata.CrimeMoney > 0 || bndata.GangSoftcap * bndata.GangUniqueAugs > 0)
-					BankStrat('op_task-manager.js');
-				BankStrat('op_bupgr.js');
-				if (bndata.HacknetNodeMoney > 0)
-					BankStrat('op_bhnodes.js');
-				if (bndata.GangSoftcap * bndata.GangUniqueAugs > 0)
+				if (allowGang && bndata.GangSoftcap * bndata.GangUniqueAugs > 0)
 					BankStrat('lp_gang.js');
-				BankStrat('op_sleeve.js');
+				if (allowSleeves)
+					BankStrat('op_sleeve.js');
 				if (player.money >= 2e6)
 					BankStrat('lp_stockmaster.js');
-				BankStrat('op_joinfacs.js');
-				BankStrat('op_graft.js');
-
 				BankStrat('mp_bladeburner.js');
-
-				if (bndata.CodingContractMoney > 0)
-					BankStrat('cct_solve.js');
 				BankStrat('lp_svhack.js');
 				BankStrat('mp_nuke-backdoor.js');
+				BankStrat('op_joinfacs.js');
+				if (allowGraft)
+					BankStrat('op_graft.js');
 				if (bndata.PurchasedServerLimit > 0)
 					BankStrat('op_bservs.js');
-				if (bndata.CorporationValuation * bndata.CorporationSoftcap > 0)
-					BankStrat('mp_corp.js');
-				if (GLOBAL_VARS["hackMult"] > 0)
-					BankStrat('lp_loopmaster.js');
-
-				if (player.skills.hacking >= 3000 * bndata.WorldDaemonDifficulty || GLOBAL_VARS["bb"]["isComplete"]) {
-					let myAugs = await nstb.RunCom(ns, 'ns.singularity.getOwnedAugmentations()');
-					if (myAugs.includes("The Red Pill") || GLOBAL_VARS["bb"]["isComplete"]) { BankStrat('endBitNode.js') }
-				};
 				break;
+			case "redpill":
+				if (player.money >= 2e6)
+					BankStrat('lp_stockmaster.js');
+				if (bndata.PurchasedServerLimit > 0)
+					BankStrat('op_bservs.js');
+				BankStrat('lp_svhack.js');
+				BankStrat('mp_nuke-backdoor.js');
+				BankStrat('op_joinfacs.js');
+				if (allowSleeves)
+					BankStrat('op_sleeve.js');
+				if (allowGraft)
+					BankStrat('op_graft.js');
+				if (allowGang && bndata.GangSoftcap * bndata.GangUniqueAugs > 0)
+					BankStrat('lp_gang.js');
+				break;
+			default:
+				throw new Error("Invalid runType!");
 		}
+
+		if (bndata.CodingContractMoney > 0)
+			BankStrat('cct_solve.js');
+		if (allowStanek)
+			BankStrat('lp_stanek.js');
+		if (allowCorp && bndata.CorporationValuation * bndata.CorporationSoftcap > 0)
+			BankStrat('mp_corp.js');
+		if (GLOBAL_VARS["hackMult"] > 0)
+			BankStrat('lp_loopmaster.js');
+
+		if (player.skills.hacking >= 3000 * bndata.WorldDaemonDifficulty || GLOBAL_VARS["bb"]["isComplete"]) {
+			let myAugs = await nstb.RunCom(ns, 'ns.singularity.getOwnedAugmentations()');
+			if (myAugs.includes("The Red Pill") || GLOBAL_VARS["bb"]["isComplete"]) { BankStrat('endBitNode.js') }
+		};
+
 		// ========================================================================================================================
 		// ========================================================================================================================
 
@@ -260,7 +278,7 @@ export async function main(ns) {
 			return true;
 		}
 		// else, if not in BN-9 and other income >= hnserv income, do the same checks to see if we can run it on hacknet-node-0
-		else if (numnodes > 0 && bitNode != 9 && tb.SumDict(GLOBAL_VARS["income"]) - (2 * GLOBAL_VARS["income"]["hacknet"]) > 0) {
+		else if (allowHacknet && numnodes > 0 && bitNode != 9 && tb.SumDict(GLOBAL_VARS["income"]) - (2 * GLOBAL_VARS["income"]["hacknet"]) > 0) {
 			// if already running (will be the case for loops / lp_ files)
 			if (ns.isRunning(filename, 'hacknet-node-0')) {
 				hnscriptsSize += ns.getScriptRam(filename); // Account for the RAM usage
