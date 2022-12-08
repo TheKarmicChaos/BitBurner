49a9698d2af3f5984f8666264b3763b7569142f8
TheKarmicChaos <73192767+TheKarmicChaos@users.noreply.github.com>
2022-12-08 14:47:01 -0500

    Reorder & reorganize funcs in prep for batcher

--- src/lp_svhack.js
+++ src/lp_svhack.js
@@ -6,8 +6,212 @@ export async function main(ns) {
 	ns.closeTail(); await ns.sleep(1); ns.tail('lp_svhack.js'); await ns.sleep(1); ns.resizeTail(340, 295); await ns.sleep(1); ns.moveTail(1540, 155);
 	ns.disableLog("ALL"); ns.clearLog();
 
-	let hackInfluencesStocks = false
-	let growInfluencesStocks = true
+	// Declare vars & consts
+	let targetCoef = 0.5;
+	let allServs = nstb.ScanAll(ns, true)[0];
+	let allTargetServs;
+	let allUsableServs = FindAllUsable(allServs);
+	let homeSize;
+	const hackSize = await nstb.RunCom(ns, 'ns.getScriptRam()', ['smart-hack.js']);
+	const chargeSize = await nstb.RunCom(ns, 'ns.getScriptRam()', ['smart-charge.js']);
+
+	// Settings
+	const hackInfluencesStocks = false
+	const growInfluencesStocks = true
+	
+	KillEverything(allUsableServs);
+
+	var beingHacked = {};
+
+	while (true) { await HackLoop() }
+
+	async function HackLoop() {
+
+		// Update variables
+		allServs = nstb.ScanAll(ns, true)[0];
+		allTargetServs = await FindAllTargets(allServs);
+		allUsableServs = FindAllUsable(allServs);
+		homeSize = await nstb.RunCom(ns, 'ns.getServerMaxRam()', ['home']);
+		
+		// Figure out what fraction of our home RAM should be left unused in order to run core scripts
+		let coreCoef = 0.5;
+		if (homeSize <= 2048) coreCoef = 1;
+		let maxRamForCore = homeSize * coreCoef;
+
+		// Decrement all second (tick) counts for servers in beingHacked, removing them from the dict if their ticks value is below 0
+		// (as this means the hack/weak/grow done to this server is finished).
+		for (let key in beingHacked) beingHacked[key][0] -= 1;
+
+		let forcehack = false;
+
+		// Iterate through every targetServ (starting with best target), descending the array from best to worst
+		for (var i = (allTargetServs.length - 1); i >= 0; --i) {
+			var targetservname = allTargetServs[i];
+			// IF this targetServ is NOT in "beingHacked"
+			if (!(IsBeingHacked(targetservname))) {
+				ns.print(targetservname)
+
+				// Get required threads
+				let allRequiredThreads = await GetRequiredThreads(targetservname);
+
+				// Pick an action for this server
+				let action = await ChooseAction(targetservname, forcehack);
+				let actionID = action[0];
+				var actionTime = action[1];
+				ns.print("ACTION: " + actionID);
+				let stockOpt = false;
+				if (actionID == 1) { stockOpt = growInfluencesStocks }
+				else if (actionID == 2) { stockOpt = hackInfluencesStocks }
+
+				// Get the required threads for the chosen action
+				var requiredThr = allRequiredThreads[actionID];
+				var requiredThrHome = allRequiredThreads[actionID + 3];
+				if (requiredThr <= 0) { requiredThr = 1 }
+				if (requiredThrHome <= 0) { requiredThrHome = 1 }
+				// For every usableServ in array of allUsableServs (ascending from worst to best)
+				for (var x = 0; x < allUsableServs.length; ++x) {
+					var usableservname = allUsableServs[x];
+					ns.print(targetservname)
+					ns.print(usableservname)
+
+					// IF the targetserv is not yet in "beingHacked"
+					if (!(IsBeingHacked(targetservname))) {
+						var freeRam;
+						if (usableservname == 'home') {
+							ns.print("needs " + requiredThrHome + " for " + actionID)
+							freeRam = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
+							freeRam -= maxRamForCore;
+						} else {
+							ns.print("needs " + requiredThr + " for " + actionID)
+							freeRam = (ns.getServerMaxRam(usableservname) - ns.getServerUsedRam(usableservname))
+						}
+						var freeThr = Math.floor(freeRam / hackSize);
+						ns.print(" HAS " + freeThr)
+						ns.print("---------")
+
+						// if the usableserv has enough free space for requiredThr
+						var reqThrFinal;
+						if (usableservname == 'home') { reqThrFinal = requiredThrHome }
+						else { reqThrFinal = requiredThr }
+
+
+						if (freeThr >= reqThrFinal) {
+							var ticksUntilDone = Math.ceil(actionTime / 1000)
+
+							// add targetserv to "beingHacked" dict (w/ value as [# of ticks until done, action ID, pid of script running it])
+							
+							await AttackServ(usableservname, reqThrFinal, targetservname, actionID, stockOpt);
+							//let income = ns.getScriptIncome('smart-hack.js', usableservname, [thrToUse, targetservname, actionID])
+							beingHacked[targetservname] = [ticksUntilDone, actionID];
+						}
+						//await ns.sleep(1000)
+					}
+				}
+			}
+		}
+
+		for (var i = (allTargetServs.length - 1); i >= 0; --i) {
+			var targetservname = allTargetServs[i];
+			// IF this targetServ is NOT in "beingHacked"
+			if (!IsBeingHacked(targetservname)) {
+				ns.print(targetservname)
+
+				// Get required threads
+				let allRequiredThreads = await GetRequiredThreads(targetservname);
+
+				// Pick an action for this server
+				let action = await ChooseAction(targetservname, forcehack);
+				let actionID = action[0];
+				ns.print("ACTION: " + actionID);
+				var actionTime = action[1];
+				let stockOpt = false;
+				if (actionID == 1) { stockOpt = growInfluencesStocks }
+				else if (actionID == 2) { stockOpt = hackInfluencesStocks }
+
+				// Get the required threads for the chosen action
+				var requiredThr = allRequiredThreads[actionID];
+				if (requiredThr <= 0) { requiredThr = 1 }
+				// For every usableServ in array of allUsableServs (descending from best to worst)
+				for (var x = allUsableServs.length - 1; x >= 0; --x) {
+					var usableservname = allUsableServs[x];
+					ns.print("Target: ", targetservname)
+					ns.print("Usable: ", usableservname)
+
+					// IF the targetserv is not yet in "beingHacked"
+					var freeRam;
+					ns.print("needs " + requiredThr + " for " + actionID)
+					if (usableservname == 'home') {
+						freeRam = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
+						freeRam -= maxRamForCore;
+					} else { freeRam = (ns.getServerMaxRam(usableservname) - ns.getServerUsedRam(usableservname)) }
+					var freeThr = Math.floor(freeRam / hackSize);
+					ns.print(" HAS " + freeThr)
+					ns.print("-----(filling)----")
+
+
+					// if the usableserv has enough free space for requiredThr
+					if (freeThr >= requiredThr) {
+						//if (thrToUse == 0) { ns.toast("ERROR: thrtouse=0", "error", 2000); await ns.sleep(10000);  } else {
+						var thrToUse = Math.min(freeThr, requiredThr)
+						var ticksUntilDone = Math.ceil(actionTime / 1000)
+
+						// add targetserv to "beingHacked" dict (w/ value as [# of ticks until done, action ID])
+						beingHacked[targetservname] = [ticksUntilDone, (actionID + 3)];
+						await AttackServ(usableservname, freeThr, targetservname, actionID, stockOpt);
+						//}
+					} else if (freeThr > 0) {
+						await AttackServ(usableservname, freeThr, targetservname, actionID, stockOpt);
+						requiredThr -= freeThr;
+					}
+
+					if (x == 0) {
+						var ticksUntilDone = Math.ceil(actionTime / 1000)
+						beingHacked[targetservname] = [ticksUntilDone, (actionID + 3)];
+					}
+					//await ns.sleep(1000)
+				}
+			}
+
+		}
+
+		// print debug stats for log
+		var printLines = [""];
+		var actionName;
+		//ns.run('hud.js');
+		ns.clearLog();
+		for (var x = 0; x < allTargetServs.length; ++x) {
+			let servname = allTargetServs[x];
+			if (IsBeingHacked(servname)) {
+				var timeLeft = beingHacked[servname][0];
+				let actionID = beingHacked[servname][1];
+				var actionName;
+				if (actionID == 0) {
+					actionName = ' Weak'
+				} else if (actionID == 1) {
+					actionName = ' Grow'
+				} else if (actionID == 2) {
+					actionName = ' Hack'
+				} else if (actionID == 3) {
+					actionName = '*Weak'
+				} else if (actionID == 4) {
+					actionName = '*Grow'
+				} else if (actionID == 5) {
+					actionName = '*Hack'
+				} else { actionName = 'ERROR' }
+				var prStr = tb.TextFloat(34, actionName + " [" + tb.StandardTime(timeLeft) + "]", " ", servname)
+				printLines.push(prStr);
+			} else {
+				let prStr = tb.TextFloat(34, " - INACTIVE - ", " ", servname)
+				printLines.push(prStr);
+			}
+		}
+		for (var x = 0; x < printLines.length; ++x) { ns.print(printLines[x]) }
+
+		// Update income in globals
+		let hackincome = ns.getScriptIncome('lp_svhack.js', 'home');
+		nstb.updGlobals(ns, ["income.hacking", hackincome]);
+		await ns.sleep(1000);
+	}
 
 	async function FindAllTargets(servs) {
 		var all = ['n00dles'];
@@ -153,219 +357,6 @@ export async function main(ns) {
 				return true
 			}
 		} else { return false }
-	}
-
-	var allServs = nstb.ScanAll(ns, true)[0];
-
-	var targetCoef = 0.5;
-	var allTargetServs = await FindAllTargets(allServs);
-	var allUsableServs = FindAllUsable(allServs);
-	KillEverything(allUsableServs);
-
-	var beingHacked = {};
-
-	while (true) { await BetterLoop() }
-
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	// BETTER LOOP -------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-	// ------------------------------------------------------------------------
-
-
-	async function BetterLoop() {
-
-		// initialize/update variables
-		allServs = nstb.ScanAll(ns, true)[0];
-		allTargetServs = await FindAllTargets(allServs);
-		allUsableServs = FindAllUsable(allServs);
-
-		let homeSize = await nstb.RunCom(ns, 'ns.getServerMaxRam()', ['home']);
-		let hackSize = await nstb.RunCom(ns, 'ns.getScriptRam()', ['smart-hack.js']);
-		let coreCoef;
-		if (homeSize <= 2048) { coreCoef = 1 }
-		else { coreCoef = 0.5 }
-		let maxRamForCore = homeSize * coreCoef;
-
-		// decrement all tick counts for servers in beingHacked
-		// removing them from the dict if their ticks value is below 0
-		// as this means the hack/weak/grow done to this server is finished.
-		// NOTE: 1 tick = 1 second
-		for (let key in beingHacked) {
-			beingHacked[key][0] -= 1;
-		}
-
-		let forcehack = false;
-
-		// Iterate through every targetServ (starting with best target), descending the array from best to worst
-		for (var i = (allTargetServs.length - 1); i >= 0; --i) {
-			var targetservname = allTargetServs[i];
-			// IF this targetServ is NOT in "beingHacked"
-			if (!(IsBeingHacked(targetservname))) {
-				ns.print(targetservname)
-
-				// Get required threads
-				let allRequiredThreads = await GetRequiredThreads(targetservname);
-
-				// Pick an action for this server
-				let action = await ChooseAction(targetservname, forcehack);
-				let actionID = action[0];
-				var actionTime = action[1];
-				ns.print("ACTION: " + actionID);
-				let stockOpt = false;
-				if (actionID == 1) { stockOpt = growInfluencesStocks }
-				else if (actionID == 2) { stockOpt = hackInfluencesStocks }
-
-				// Get the required threads for the chosen action
-				var requiredThr = allRequiredThreads[actionID];
-				var requiredThrHome = allRequiredThreads[actionID + 3];
-				if (requiredThr <= 0) { requiredThr = 1 }
-				if (requiredThrHome <= 0) { requiredThrHome = 1 }
-				// For every usableServ in array of allUsableServs (ascending from worst to best)
-				for (var x = 0; x < allUsableServs.length; ++x) {
-					var usableservname = allUsableServs[x];
-					ns.print(targetservname)
-					ns.print(usableservname)
-
-					// IF the targetserv is not yet in "beingHacked"
-					if (!(IsBeingHacked(targetservname))) {
-						var freeRam;
-						if (usableservname == 'home') {
-							ns.print("needs " + requiredThrHome + " for " + actionID)
-							freeRam = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
-							freeRam -= maxRamForCore;
-						} else {
-							ns.print("needs " + requiredThr + " for " + actionID)
-							freeRam = (ns.getServerMaxRam(usableservname) - ns.getServerUsedRam(usableservname))
-						}
-						var freeThr = Math.floor(freeRam / hackSize);
-						ns.print(" HAS " + freeThr)
-						ns.print("---------")
-
-						// if the usableserv has enough free space for requiredThr
-						var reqThrFinal;
-						if (usableservname == 'home') { reqThrFinal = requiredThrHome }
-						else { reqThrFinal = requiredThr }
-
-
-						if (freeThr >= reqThrFinal) {
-							var ticksUntilDone = Math.ceil(actionTime / 1000)
-
-							// add targetserv to "beingHacked" dict (w/ value as [# of ticks until done, action ID, pid of script running it])
-							
-							await AttackServ(usableservname, reqThrFinal, targetservname, actionID, stockOpt);
-							//let income = ns.getScriptIncome('smart-hack.js', usableservname, [thrToUse, targetservname, actionID])
-							beingHacked[targetservname] = [ticksUntilDone, actionID];
-						}
-						//await ns.sleep(1000)
-					}
-				}
-			}
-		}
-
-		for (var i = (allTargetServs.length - 1); i >= 0; --i) {
-			var targetservname = allTargetServs[i];
-			// IF this targetServ is NOT in "beingHacked"
-			if (!IsBeingHacked(targetservname)) {
-				ns.print(targetservname)
-				//beingHacked[targetservname] = [0, 7]
-				// Set up variables???
-
-				// Get required threads
-				let allRequiredThreads = await GetRequiredThreads(targetservname);
-
-				// Pick an action for this server
-				let action = await ChooseAction(targetservname, forcehack);
-				let actionID = action[0];
-				ns.print("ACTION: " + actionID);
-				var actionTime = action[1];
-				let stockOpt = false;
-				if (actionID == 1) { stockOpt = growInfluencesStocks }
-				else if (actionID == 2) { stockOpt = hackInfluencesStocks }
-
-				// Get the required threads for the chosen action
-				var requiredThr = allRequiredThreads[actionID];
-				if (requiredThr <= 0) { requiredThr = 1 }
-				// For every usableServ in array of allUsableServs (descending from best to worst)
-				for (var x = allUsableServs.length - 1; x >= 0; --x) {
-					var usableservname = allUsableServs[x];
-					ns.print("Target: ", targetservname)
-					ns.print("Usable: ", usableservname)
-
-					// IF the targetserv is not yet in "beingHacked"
-					var freeRam;
-					ns.print("needs " + requiredThr + " for " + actionID)
-					if (usableservname == 'home') {
-						freeRam = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
-						freeRam -= maxRamForCore;
-					} else { freeRam = (ns.getServerMaxRam(usableservname) - ns.getServerUsedRam(usableservname)) }
-					var freeThr = Math.floor(freeRam / hackSize);
-					ns.print(" HAS " + freeThr)
-					ns.print("-----(filling)----")
-
-
-					// if the usableserv has enough free space for requiredThr
-					if (freeThr >= requiredThr) {
-						//if (thrToUse == 0) { ns.toast("ERROR: thrtouse=0", "error", 2000); await ns.sleep(10000);  } else {
-						var thrToUse = Math.min(freeThr, requiredThr)
-						var ticksUntilDone = Math.ceil(actionTime / 1000)
-
-						// add targetserv to "beingHacked" dict (w/ value as [# of ticks until done, action ID])
-						beingHacked[targetservname] = [ticksUntilDone, (actionID + 3)];
-						await AttackServ(usableservname, freeThr, targetservname, actionID, stockOpt);
-						//}
-					} else if (freeThr > 0) {
-						await AttackServ(usableservname, freeThr, targetservname, actionID, stockOpt);
-						requiredThr -= freeThr;
-					}
-
-					if (x == 0) {
-						var ticksUntilDone = Math.ceil(actionTime / 1000)
-						beingHacked[targetservname] = [ticksUntilDone, (actionID + 3)];
-					}
-					//await ns.sleep(1000)
-				}
-			}
-
-		}
-
-		// print debug stats for log
-		var printLines = [""];
-		var actionName;
-		//ns.run('hud.js');
-		ns.clearLog();
-		for (var x = 0; x < allTargetServs.length; ++x) {
-			let servname = allTargetServs[x];
-			if (IsBeingHacked(servname)) {
-				var timeLeft = beingHacked[servname][0];
-				let actionID = beingHacked[servname][1];
-				var actionName;
-				if (actionID == 0) {
-					actionName = ' Weak'
-				} else if (actionID == 1) {
-					actionName = ' Grow'
-				} else if (actionID == 2) {
-					actionName = ' Hack'
-				} else if (actionID == 3) {
-					actionName = '*Weak'
-				} else if (actionID == 4) {
-					actionName = '*Grow'
-				} else if (actionID == 5) {
-					actionName = '*Hack'
-				} else { actionName = 'ERROR' }
-				var prStr = tb.TextFloat(34, actionName + " [" + tb.StandardTime(timeLeft) + "]", " ", servname)
-				printLines.push(prStr);
-			} else {
-				let prStr = tb.TextFloat(34, " - INACTIVE - ", " ", servname)
-				printLines.push(prStr);
-			}
-		}
-		for (var x = 0; x < printLines.length; ++x) { ns.print(printLines[x]) }
-
-		// Update income in globals
-		let hackincome = ns.getScriptIncome('lp_svhack.js', 'home');
-		nstb.updGlobals(ns, ["income.hacking", hackincome]);
-		await ns.sleep(1000);
 	}
 
 	async function AttackServ(hostServ, threadsToUse, targetServ, actionID, stockOpt) {
